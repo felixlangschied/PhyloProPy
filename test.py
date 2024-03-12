f69a9f0e2f30e7c738950e2503896efc9f28edb6
felixlangschied <langschied@bio.uni-frankfurt.de>
2024-03-12 09:50:27 +0100

    Added testdata

--- test.py
+++
@@ -1,6 +0,0 @@
-from PhyloProPy.PhyloProfile import PhyloProfile
-
-
-testpath = '/share/gluster/Projects/felix/weichert/fdog/phyloprofiles/RRf_AAf.phyloprofile'
-pp = PhyloProfile(testpath)
-pp.print()
